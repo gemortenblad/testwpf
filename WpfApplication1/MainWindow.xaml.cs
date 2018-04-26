2fa96159a591b4e89a826d5b4332d50d6e7783ce
Mattias <m_ortenblad@hotmail.com>
2018-04-26 15:00:13 +0200

    initial

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public string MyText = "Click";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, MouseButtonEventArgs e)
+        {
+            var textBlock = ((TextBlock) sender);
+            textBlock.Text = textBlock.Text == MyText ? MyText + "ed again" : MyText;
+        }
+    }
+}
